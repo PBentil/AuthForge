a012048b7c09f252e443378092c84b29f5a79e0e
PBentil <paapabentil122@gmail.com>
2026-09-13 11:04:32 +0000

    feat: jwt service

--- src/main/java/com/authforge/authforge/controller/AuthController.java
+++ src/main/java/com/authforge/authforge/controller/AuthController.java
@@ -33,7 +33,9 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request){
+    public ResponseEntity<LoginResponse> login(
+            @Valid @RequestBody LoginRequest request
+    ) {
         LoginResponse response = authService.login(request);
 
         return ResponseEntity.ok(response);
--- src/main/java/com/authforge/authforge/dto/LoginResponse.java
+++ src/main/java/com/authforge/authforge/dto/LoginResponse.java
@@ -2,12 +2,18 @@ package com.authforge.authforge.dto;
 
 public class LoginResponse {
     private String Message;
+    private String token;
 
-    public LoginResponse(String message) {
+    public LoginResponse(String message, String token) {
         Message = message;
+        this.token = token;
     }
 
     public String getMessage() {
         return Message;
+    }
+
+    public String getToken() {
+        return token;
     }
 }
